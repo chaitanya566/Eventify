ba14dd9b3390f06baaaf1de5a2856b5406e16cb3
chaitanya566 <120102956+chaitanya566@users.noreply.github.com>
2024-09-04 21:02:36 +0530

    Added theme functionality and added 3 themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import "./App.css";
 import Calendar from "./components/Calendar/Calendar";
 import CalendarMonthReducer from "src/Contexts/calendarCurrentMonth/calendarMonthReducer";
+import "src/assets/themes/neon-red-theme.css"
 function App() {
 
   return (
