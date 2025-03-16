e040571f79168b43be5afb2fee59328cb5a90dd5
chaitanya566 <120102956+chaitanya566@users.noreply.github.com>
2025-03-16 21:43:34 +0530

    Fix JSX runtime issue by ensuring Vite auto-imports React

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/Eventify/",
+  plugins: [react()],
   resolve: {
     alias: {
       src: "/src",
